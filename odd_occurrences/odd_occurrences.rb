ecfd36ae94f35d322115a522d49f05c6867051cb
Aneta Wozniak <wozniakaneta5@gmail.com>
2017-06-30 11:20:26 +0200

    Remove readme.md file and refactor code

--- odd_occurrences/odd_occurrences.rb
+++ odd_occurrences/odd_occurrences.rb
@@ -1,8 +1,4 @@
 def solution(array)
-  numbers = Hash.new(0)
-  odd_numbers = Hash.new(0)
-
-  numbers = array.group_by { |num| num }
-
-  odd_numbers = numbers.each_value { |value| return value[0] if value.count.odd? }
+  numbers = array.group_by(&:itself)
+  numbers.each_value { |value| return value[0] if value.count.odd? }
 end
