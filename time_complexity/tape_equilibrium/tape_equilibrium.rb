72c929b62d82bfd8436761dfcd025d6d4d5671f2
Aneta Wozniak <wozniakaneta5@gmail.com>
2018-05-14 12:51:55 +0200

    Add solution for Tape Equilibrium

---
+++ time_complexity/tape_equilibrium/tape_equilibrium.rb
@@ -0,0 +1,13 @@
+def solution(a)
+  lside = a[0]
+  rside = a.slice(1..-1).inject(:+)
+  min_diff = (lside - rside).abs
+  (1..a.count-2).each{|index|
+    lside += a[index]
+    rside -= a[index]
+    if (lside - rside).abs < min_diff
+      min_diff = (lside - rside).abs
+    end
+  }
+  min_diff
+end
