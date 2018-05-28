ff89390b32ff72837b6646003a7072f5a5d23d20
Aneta Wozniak <wozniakaneta5@gmail.com>
2018-05-28 11:30:22 +0200

    Add refactor

--- time_complexity/tape_equilibrium/tape_equilibrium.rb
+++ time_complexity/tape_equilibrium/tape_equilibrium.rb
@@ -2,12 +2,12 @@ def solution(a)
   lside = a[0]
   rside = a.slice(1..-1).inject(:+)
   min_diff = (lside - rside).abs
-  (1..a.count-2).each{|index|
+  (1..a.count - 2).each do |index|
     lside += a[index]
     rside -= a[index]
     if (lside - rside).abs < min_diff
       min_diff = (lside - rside).abs
     end
-  }
+  end
   min_diff
 end
